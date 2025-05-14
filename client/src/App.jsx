161492b952e1234cc6e9ce7dfaf653169560a4d0
Chanisha <csachdeva83@gmail.com>
2025-05-14 16:47:00 +0530

    Fix bingo game

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("http://localhost:3002");
 
 const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
